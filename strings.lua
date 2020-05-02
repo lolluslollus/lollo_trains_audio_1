d5dc02e049bb484feb1b31496663c826382ab5f2
lolluslollus <lolluslollus@outlook.com>
2020-05-02 22:07:39 +0200

    workshop_fileid.txt

--- strings.lua
+++ strings.lua
@@ -1,7 +1,7 @@
 function data()
     return {
         en = {
-            ['_DESC'] = 'Station masters whistle when (several models of) train leave./n'
+            ['_DESC'] = 'Station masters whistle when (several models of) train leave.\n'
             .. 'Certain local trains receive German underground announcements, as a bonus.'
         },
         en = {
