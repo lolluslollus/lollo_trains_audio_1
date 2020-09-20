3a03ec3ac9111e4045deb0c4c7306ef163aefda6
lolluslollus <lolluslollus@outlook.com>
2020-09-20 20:03:23 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -2,8 +2,10 @@ function data()
     return {
         en = {
             ['_DESC'] = [[
-                [h2]Station masters whistle when (several models of) train leave.[/h2]
-                Certain local trains receive German underground announcements, as a bonus.
+                [h2]Station masters whistle when trains leave.[/h2]
+                This mod works for vanilla and many modded trains.
+                Certain local trains receive German or English underground announcements, as a bonus.
+
                 You can customise this mod with <mod directory>/res/scripts/lollo_trains_audio/settings.lua.
                 For example, you can make it say "mind the gap" and "stand clear of the doors", or nothing at all.
                 You can also add your own sounds. Just play around.
