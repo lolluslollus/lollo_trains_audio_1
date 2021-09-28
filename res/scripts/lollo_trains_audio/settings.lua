3f5859907654b9a50a0a3b048f890d87621647c2
lolluslollus <lolluslollus@users.noreply.github.com>
2021-09-28 23:05:24 +0200

    new german open and close door sounds and new settings to reflect them

--- res/scripts/lollo_trains_audio/settings.lua
+++ res/scripts/lollo_trains_audio/settings.lua
@@ -35,18 +35,18 @@ return {
     -- Every tuple contains a part of the sound set file name and the path of the wav file, relative to res/audio/effects/
     -- Mods store their sound sets in <mod directory>/res/config/sound_set .
     soundSetsThatReceiveTheCloseDoorsSound = {
-        {soundSetName = '/1044_sound.lua', wavName = 'closeDoors/bitte-zurueckbleiben-02.wav'},
+        {soundSetName = '/1044_sound.lua', wavName = 'closeDoors/bitte-zurueckbleiben-01-w-door-beep.wav'},
         {soundSetName = '/1992_stock_sound.lua', wavName = 'closeDoors/stand-clear-of-the-doors-02.wav'}, -- london UG 1992 stock
-        -- {soundSetName = '/br_1440_', wavName = 'closeDoors/bitte-zurueckbleiben-02.wav'}, -- too much
+        -- {soundSetName = '/br_1440_', wavName = 'closeDoors/bitte-zurueckbleiben-01-w-door-beep.wav'}, -- too much
         {soundSetName = '/br_1440_waggon_db.lua', wavName = 'closeDoors/bitte-zurueckbleiben-01.wav'},
-        {soundSetName = '/br_1440_waggon_enno.lua', wavName = 'closeDoors/bitte-zurueckbleiben-01.wav'},
+        {soundSetName = '/br_1440_waggon_enno.lua', wavName = 'closeDoors/bitte-zurueckbleiben-02.wav'},
         {soundSetName = '/g_metro.lua', wavName = 'closeDoors/stand-clear-of-the-doors-02.wav'},
         {soundSetName = '/S8.lua', wavName = 'closeDoors/stand-clear-of-the-doors-01.wav'}, -- london UG S8 stock
         {soundSetName = '/StadlerWaggon.lua', wavName = 'closeDoors/bitte-zurueckbleiben-01.wav'},
         {soundSetName = '/StadlerWaggon2.lua', wavName = 'closeDoors/bitte-zurueckbleiben-01.wav'},
-        {soundSetName = '/talent2_front.lua', wavName = 'closeDoors/bitte-zurueckbleiben-02.wav'},
-        -- {soundSetName = '/talent2_middle.lua', wavName = 'closeDoors/bitte-zurueckbleiben-02.wav'},
-        {soundSetName = '/train_trieb_br423w.lua', wavName = 'closeDoors/stand-clear-of-the-doors-01.wav'}, -- br 422, 423, 430
+        {soundSetName = '/talent2_front.lua', wavName = 'closeDoors/bitte-zurueckbleiben-01-w-door-beep.wav'},
+        -- {soundSetName = '/talent2_middle.lua', wavName = 'closeDoors/bitte-zurueckbleiben-01-w-door-beep.wav'},
+        {soundSetName = '/train_trieb_br423w.lua', wavName = 'closeDoors/bitte-zurueckbleiben-01.wav'}, -- br 422, 423, 430
         -- {soundSetName = '/train_trieb_br423stw.lua', wavName = 'closeDoors/bitte-zurueckbleiben-01.wav'}, -- br 422, 423, 430
         {soundSetName = '/z8100.lua', wavName = 'closeDoors/stand-clear-of-the-doors-02.wav'}, -- RATP and RER
         {soundSetName = '/z8100R.lua', wavName = 'closeDoors/stand-clear-of-the-doors-02.wav'}, -- RATP and RER
@@ -56,19 +56,19 @@ return {
     -- Every tuple contains a part of the sound set file name and the path of the wav file, relative to res/audio/effects/
     -- Mods store their sound sets in <mod directory>/res/config/sound_set .
     soundSetsThatReceiveTheOpenDoorsSound = {
-        {soundSetName = '/1044_sound.lua', wavName = 'openDoors/mind-the-gap-03.wav'},
+        {soundSetName = '/1044_sound.lua', wavName = 'openDoors/bitte-beachten-sie-die-ansagen-w-gong.wav'},
         {soundSetName = '/1992_stock_sound.lua', wavName = 'openDoors/mind-the-gap-02.wav'}, -- london 1992 stock
         -- {soundSetName = '/br_1440_', wavName = 'openDoors/mind-the-gap-01.wav'}, -- too much
-        {soundSetName = '/br_1440_waggon_db.lua', wavName = 'openDoors/mind-the-gap-01.wav'},
-        {soundSetName = '/br_1440_waggon_enno.lua', wavName = 'openDoors/mind-the-gap-01.wav'},
+        {soundSetName = '/br_1440_waggon_db.lua', wavName = 'openDoors/bitte-achten-auf-abstand.wav'},
+        {soundSetName = '/br_1440_waggon_enno.lua', wavName = 'openDoors/bitte-benutzen-sie-alle-einstiegstueren.wav'},
         {soundSetName = '/g_metro.lua', wavName = 'openDoors/mind-the-gap-01.wav'},
         {soundSetName = '/S8.lua', wavName = 'openDoors/mind-the-gap-03.wav'}, -- london S8 stock
-        {soundSetName = '/StadlerWaggon.lua', wavName = 'openDoors/mind-the-gap-03.wav'},
-        {soundSetName = '/StadlerWaggon2.lua', wavName = 'openDoors/mind-the-gap-03.wav'},
-        {soundSetName = '/talent2_front.lua', wavName = 'openDoors/mind-the-gap-03.wav'},
-        -- {soundSetName = '/talent2_middle.lua', wavName = 'openDoors/mind-the-gap-03.wav'},
-        {soundSetName = '/train_trieb_br423w.lua', wavName = 'openDoors/mind-the-gap-03.wav'}, -- br 422, 423, 430
-        -- {soundSetName = '/train_trieb_br423stw.lua', wavName = 'openDoors/mind-the-gap-03.wav'}, -- br 422, 423, 430
+        {soundSetName = '/StadlerWaggon.lua', wavName = 'openDoors/bitte-beachten-sie-die-ansagen.wav'},
+        {soundSetName = '/StadlerWaggon2.lua', wavName = 'openDoors/bitte-beachten-sie-die-ansagen.wav'},
+        {soundSetName = '/talent2_front.lua', wavName = 'openDoors/bitte-benutzen-sie-alle-einstiegstueren-w-beep.wav'},
+        -- {soundSetName = '/talent2_middle.lua', wavName = 'openDoors/bitte-benutzen-sie-alle-einstiegstueren-w-beep.wav'},
+        {soundSetName = '/train_trieb_br423w.lua', wavName = 'openDoors/bitte-achten-auf-abstand.wav'}, -- br 422, 423, 430
+        -- {soundSetName = '/train_trieb_br423stw.lua', wavName = 'openDoors/bitte-achten-auf-abstand.wav'}, -- br 422, 423, 430
         {soundSetName = '/z8100.lua', wavName = 'openDoors/mind-the-gap-01.wav'}, -- RATP and RER
         {soundSetName = '/z8100R.lua', wavName = 'openDoors/mind-the-gap-01.wav'}, -- RATP and RER
     }
