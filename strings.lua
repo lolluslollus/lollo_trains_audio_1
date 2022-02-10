8c5ef9028a8618129470054fa8cf660b37234ccb
lolluslollus <lolluslollus@hotmail.com>
2022-02-10 10:24:22 +0100

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -2,9 +2,10 @@ function data()
     return {
         en = {
             ['_DESC'] = [[
-                [h2]Station masters whistle when trains leave.[/h2]
+                [b]Station masters whistle when trains leave.
+                Local trains instead receive announcements when opening and closing the doors.[/b]
+
                 This mod works for vanilla and many modded trains.
-                Certain local trains receive underground announcements, as a bonus.
 
                 You can customise this mod with <mod directory>/res/scripts/lollo_trains_audio/settings.lua.
                 For example, you can make it say "mind the gap" and "stand clear of the doors", or nothing at all.
@@ -13,7 +14,7 @@ function data()
                 Note 1: the whistles can only come if the train has a sound set with clacks or chuffs.
                 This mod adds clacks to certain modded sound sets that haven't got them. It does not add chuffs.
 
-                Note 2: to get more modded trains to whistle, load https://steamcommunity.com/sharedfiles/filedetails/?id=1956333378 and https://steamcommunity.com/sharedfiles/filedetails/?id=1984933237 before other mods and this one after.
+                Note 2: to get more modded trains to whistle, load https://steamcommunity.com/sharedfiles/filedetails/?id=1956333378 and https://steamcommunity.com/sharedfiles/filedetails/?id=1984933237 [b]before[/b] other mods and this one after.
             ]],
             ['_NAME'] = 'Extra sound effects for trains'
         }
