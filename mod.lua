7c1046ef21738be130b26d76ddcc214874aea715
lolluslollus <lolluslollus@users.noreply.github.com>
2023-03-23 20:05:07 +0100

    bump

--- mod.lua
+++ mod.lua
@@ -194,7 +194,7 @@ local _whistleWavNames = {
 function data()
     return {
         info = {
-            minorVersion = 14,
+            minorVersion = 15,
             severityAdd = 'NONE',
             severityRemove = 'NONE',
             name = _('_NAME'),
